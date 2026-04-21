0d089f122112adca3d9dbd890a98a2bf7d56df39
breno30 <brenodonascimentosilvaa@gmail.com>
2026-04-21 18:50:23 -0300

    simplify output

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,4 @@
-output "cloud_drive_bucket_name" {
+output "files_bucket_name" {
   value = module.storage.cloud_drive_bucket_name
 }
 
@@ -6,22 +6,10 @@ output "frontend_bucket_name" {
   value = module.storage.frontend_bucket_name
 }
 
-output "frontend_origin" {
-  value = local.frontend_origin
-}
-
-output "frontend_redirect_uri" {
-  value = local.frontend_redirect_uri
-}
-
-output "frontend_cloudfront_domain" {
-  value = module.cdn.domain_name
+output "upload_url" {
+  value = module.uploader.upload_url
 }
 
 output "frontend_cloudfront_url" {
   value = "https://${module.cdn.domain_name}/"
-}
-
-output "upload_url" {
-  value = module.uploader.upload_url
 }
